a3f32f17dbd2b9b227e3e3228708b1f7e89242f3
kazuyaseki <track613armory@gmail.com>
2019-07-01 22:14:17 +0900

    learn getByTestId

--- js_testing/src/__tests__/react-dom.test.tsx
+++ js_testing/src/__tests__/react-dom.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import 'jest-dom/extend-expect';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import { FavoriteNumber } from '../FavoriteNumber';
 
 afterEach(() => {
@@ -12,10 +12,23 @@ afterEach(() => {
 
 test('renders a number input with a label "Favorite Number"', () => {
   const { getByLabelText, getByTestId } = render(
-    <FavoriteNumber min={0} max={10} />
+    <FavoriteNumber min={1} max={9} />
   );
   const input = getByLabelText(/favorite number/i);
   const label = getByTestId('test');
   expect(input).toHaveAttribute('type', 'number');
   expect(label).toHaveTextContent('test');
+});
+
+test('entering an invlid value shows an error message', () => {
+  const { getByLabelText, getByTestId, rerender } = render(
+    <FavoriteNumber min={1} max={9} />
+  );
+  const input = getByLabelText(/favorite number/i);
+  fireEvent.change(input, { target: { value: 10 } });
+  expect(getByTestId('error-message')).toHaveTextContent(
+    'The number is invalid'
+  );
+  rerender(<FavoriteNumber min={1} max={10} />);
+  expect(getByTestId('error-message')).toBeNull();
 });
